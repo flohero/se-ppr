ae5d4e4b3de390feb42b3e5e8ac95149c950eae2
florian weingartshofer <florian.weingartshofer@tuta.io>
2022-01-26 19:12:24 +0100

    Update e06

--- e06/main.py
+++ e06/main.py
@@ -7,6 +7,8 @@ from shutil import copy2
 import pandas as pd
 
 allowed_file_types = [".txt", ".json", ".csv", ".db", ".sqlite"]
+csv_types = [".txt", ".csv"]
+sql_types = [".sqlite", ".db"]
 
 
 # db and sqlite are both sqlite files, with different extensions
@@ -17,9 +19,6 @@ class ParsedFile:
     def __init__(self, file: pathlib.Path, df: pd.DataFrame):
         self.file = file
         self.df = df
-        os.mkdir(self.base_path())
-        os.mkdir(self.out_path())
-        copy2(file.name, self.base_path())
 
     def base_path(self) -> str:
         return self.file.stem
@@ -36,15 +35,38 @@ class ParsedFile:
     def sqlite(self):
         return os.path.join(self.out_path(), self.file.stem + ".sqlite")
 
+    def create_skeleton(self):
+        os.mkdir(self.base_path())
+        os.mkdir(self.out_path())
+        copy2(f.name, self.base_path())
+
+    def meta_info(self) -> str:
+        outputtypes = None
+        if self.file.suffix in csv_types:
+            outputtypes = [".json", ".sqlite"]
+        elif self.file.suffix == ".json":
+            outputtypes = [".csv", ".sqlite"]
+        elif self.file.suffix in sql_types:
+            outputtypes = [".json", ".csv"]
+
+        return f"""Input Type: {self.file.suffix}
+Output Types: {", ".join(outputtypes)}
+Columns: {", ".join(list(self.df.columns))}
+Rows: {len(self.df)}"""
+
     def convert_file(self):
-        if self.file.suffix not in [".txt", ".csv"]:
+        self.create_skeleton()
+        if self.file.suffix not in csv_types:
             self.df.to_csv(self.csv())
-        if self.file.suffix not in [".db", ".sqlite"]:
+        if self.file.suffix not in sql_types:
             conn = sqlite3.connect(self.sqlite())
             self.df.to_sql(self.file.stem, conn)
             conn.close()
         if self.file.suffix != ".json":
-            self.df.to_json(self.json(), orient='index')
+            self.df.to_json(self.json(), orient="index")
+
+        with open(os.path.join(self.base_path(), "information.txt"), "w") as meta:
+            meta.write(self.meta_info())
 
 
 def parse_sqlite(file: pathlib.Path, table: str) -> pd.DataFrame:
@@ -60,18 +82,18 @@ def parse_sqlite(file: pathlib.Path, table: str) -> pd.DataFrame:
         conn.close()
         raise f"{table} not found in sqlite-file {file}"
 
-    df = pd.read_sql_table(table, conn)
+    df = pd.read_sql(f"SELECT * FROM {table}", conn)
     conn.close()
     return df
 
 
 def parse_file(file: pathlib.Path, *argv) -> ParsedFile:
     df = None
-    if file.suffix in [".txt", ".csv"]:
+    if file.suffix in csv_types:
         df = pd.read_csv(file, sep=argv[0])
     elif file.suffix == ".json":
         df = pd.read_json(file)
-    elif file.suffix in [".db", ".sqlite"]:
+    elif file.suffix in sql_types:
         df = parse_sqlite(file, argv[0])
 
     return ParsedFile(file, df)
@@ -80,23 +102,32 @@ def parse_file(file: pathlib.Path, *argv) -> ParsedFile:
 if __name__ == "__main__":
     args = sys.argv
     if len(args) < 2:
-        print(
-            "First argument filename, second argument optional a table name or csv seperator"
-        )
-    path = args[1]
+        path = input("Please specify the file you want to convert: ")
+    else:
+        path = args[1]
 
     if not os.path.isfile(path):
         print(f"{path} either does not exist or is not a file")
+        exit(1)
 
-    file = pathlib.Path(path)
+    f = pathlib.Path(path)
 
-    if file.suffix not in allowed_file_types:
-        print(f"{file} cannot be converted")
+    if f.suffix not in allowed_file_types:
+        print(f"{f} cannot be converted")
 
-    pf = None
-    if len(args) == 3:
-        pf = parse_file(file, *args[2::])
-    else:
-        pf = parse_file(file)
+    arg = None
 
+    if f.suffix in csv_types:
+        if len(args) < 3:
+            arg = input("Please input the csv seperator: ")
+        else:
+            arg = args[2]
+
+    if f.suffix in sql_types:
+        if len(args) < 3:
+            arg = input("Please input the table: ")
+        else:
+            arg = args[2]
+
+    pf = parse_file(f, arg)
     pf.convert_file()
